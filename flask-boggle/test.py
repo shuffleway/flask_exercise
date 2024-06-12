4559693abedc4ea93a100f87d12dcfe54fe56a35
cngwane <cngwane@CollinsBitsPC>
2024-06-12 03:09:33 -0400

    Completed falsk-boggle test

--- flask-boggle/test.py
+++ flask-boggle/test.py
@@ -3,26 +3,30 @@ from app import app
 from flask import session
 from boggle import Boggle
 
-
 class FlaskTests(TestCase):
-
     def setUp(self):
         """Stuff to do before every test."""
-
         self.client = app.test_client()
         app.config['TESTING'] = True
 
     def test_homepage(self):
         """Make sure information is in the session and HTML is displayed"""
-
         with self.client:
-            response = self.client.get('/')
-            self.assertIn('board', session)
-            self.assertIsNone(session.get('highscore'))
-            self.assertIsNone(session.get('nplays'))
-            self.assertIn(b'<p>High Score:', response.data)
-            self.assertIn(b'Score:', response.data)
-            self.assertIn(b'Seconds Left:', response.data)
+            # Create a request context for the sub-thread
+            with app.test_request_context():
+                boggle_game = Boggle()
+                board = boggle_game.make_board()
+                session['board'] = board
+
+                response = self.client.get('/')
+                self.assertIn('board', session)
+                self.assertIsNone(session.get('highscore'))
+                self.assertIsNone(session.get('nplays'))
+                self.assertIn(b'<p>High Score:', response.data)
+                self.assertIn(b'Score:', response.data)
+                self.assertIn(b'Seconds Left:', response.data)
+
+
 
     def test_valid_word(self):
         """Test if word is valid by modifying the board in the session"""
